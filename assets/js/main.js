86a43b1010858e38ff7cd153b1e1e45b52b8a51d
JelenaIvanovic86 <milinkovic.kg@gmail.com>
2021-02-22 10:39:45 +0100

    fix
    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,6 @@
 
-var myText = $(".my-txt");
 
-var openLink = $("#open");
-var closeLink = $("#close");
-
-
-
-
-
-function openCloseText(status) {
-	if ( status == 1 ) {
-		
-		myText.slideDown(1000);
-		
-		
-	}
-	else{
-		myText.slideUp(1000);
-	}
+function myFunction() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("display");
 }
